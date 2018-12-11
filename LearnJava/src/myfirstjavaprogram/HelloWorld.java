d0320ab6fd02e2b13b63228ed7cba11041710119
ollemiljukov <ollemiljukov@olles-mbp.lan>
2018-12-11 23:17:19 +0200

    first commit

---
+++ LearnJava/src/myfirstjavaprogram/HelloWorld.java
@@ -0,0 +1,15 @@
+package myfirstjavaprogram;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hello World...");
+		System.out.println("This is my first Java program...");
+		
+		System.out.println("Hello World 2...");
+
+	}
+
+}
