5f6d15278cf412c4dc6e521f74af3be8151e58dd
ollemiljukov <ollemiljukov@olles-mbp.lan>
2018-12-11 23:25:21 +0200

    kommentaari rida maha võetud

--- LearnJava/src/myfirstjavaprogram/HelloWorld.java
+++ LearnJava/src/myfirstjavaprogram/HelloWorld.java
@@ -3,13 +3,11 @@ package myfirstjavaprogram;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		System.out.print("Hello World...");
 		System.out.println("This is my first Java program...");
 		
 		System.out.println("Hello World 2...");
-
+		
 	}
-
 }
